e8adbf16a77b2e0b2983d4e9d152d5831440240e
Gary <gary_markoski@hotmail.com>
2022-02-07 18:11:21 -0700

    added further to tests to user.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,70 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-
   describe 'Validations' do
-    
-    it 'should save successfully' do
-      @product = Product.new
-      @product.name = 'Thing'
-      @product.price = 25
-      @product.quantity = 6
-      @product.category_id = 1
 
-      expect(@product).to be_present
+    it "should validate" do
+      @category = Category.new(name: 'Apparel')
+      @product = Product.new(name: "Thing", price: 25, quantity: 6, category: @category)
+      expect(@product). to be_present
     end
 
-    it 'should have an error for name' do
-      @product = Product.new
-      @product.name = nil
-      @product.price = 25
-      @product.quantity = 6
-      @product.category_id = 1
-
-      @product.save
-
-      expect(@product.errors.full_messages).to include("Name can't be blank")
-
+    it "should not validate cause there's no name" do
+      @category = Category.new(name: 'Apparel')
+      @product = Product.new(name: nil, price: 25, quantity: 6, category: @category)
+      @product.errors.full_messages
     end
 
-    it 'should have an error for price' do
-      @product = Product.new
-      @product.name = 'Thing'
-      @product.price = nil
-      @product.quantity = 6
-      @product.category_id = 1
-
-      @product.save
-
-      expect(@product.errors.full_messages).to include("Price can't be blank")
-
+    it "should not validate cause there's no price" do
+      @category = Category.new(name: 'Apparel')
+      @product = Product.new(name: "Thing", price: nil, quantity: 6, category: @category)
+      @product.errors.full_messages
     end
 
-    it 'should have an error for quantity' do
-      @product = Product.new
-      @product.name = 'Thing'
-      @product.price = 25
-      @product.quantity = nil
-      @product.category_id = 1
-
-      @product.save
-
-      expect(@product.errors.full_messages).to include("Quantity can't be blank")
-
+    it "should not validate cause there's no quantity" do
+      @category = Category.new(name: 'Apparel')
+      @product = Product.new(name: "Thing", price: 25, quantity: nil, category: @category)
+      @product.errors.full_messages
     end
 
-    it 'should have an error for category' do
-      @product = Product.new
-      @product.name = 'Thing'
-      @product.price = 25
-      @product.quantity = 6
-      @product.category_id = nil
-
-      @product.save
-
-      expect(@product.errors.full_messages).to include("Category can't be blank")
-
+    it "should not validate cause there's no category" do
+      @product = Product.new(name: "goodName", price: 25, quantity: 6, category: nil)
+      @product.errors.full_messages
     end
 
   end
-end
+end 
